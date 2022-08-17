c05301176d07401b19d80d3f0d71489196f37efe
Taher Odeh <taher@aumix.com>
2022-08-17 08:54:17 +0300

    refactor

--- app.py
+++ app.py
@@ -1,100 +1,73 @@
 import datetime
 
 from flask import Flask, request, Response
-from bson.objectid import ObjectId
-from pymongo import MongoClient
 import settings
 from event import Event
 
 app = Flask(__name__)
 
-events_db = MongoClient(settings.MONGO_URI)[settings.MONGO_DB]
 
 @app.after_request
 def cors_allow_all(response: Response):
     if settings.DEBUG_MODE:
         response.headers.add('Access-Control-Allow-Methods', '*')
+        response.headers.add('Access-Control-Allow-Headers', '*')
         response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
+
 @app.route("/events", methods=["GET"])
 def events():
-    if "month" in request.args.to_dict():
-        query = {
-            "month": request.args.get("month")
-        }
-    elif "week" in request.args.to_dict():
-        query = {
-            "month": request.args.get("month")
-        }
-    else:
-        return _error_response("either month (Y-m) or week (week number) must be provided")
+    filters = request.args.to_dict()
+    if not filters.get("start_date") or not filters.get("end_date"):
+        return _error_response("start_date and end_date must be provided")
 
-    results = events_db.events.find(query)
-    event_objects = [Event.from_db_row(ev).to_dict() for ev in results]
-    return {"ok": True, "events": event_objects}
+    start = datetime.datetime.strptime(filters.get("start_date"), Event.FMT_DATE)
+    end = datetime.datetime.strptime(filters.get("end_date"), Event.FMT_DATE)
+
+    return {"ok": True, "events": list(map(lambda e: e.to_dict(), Event.find(start, end)))}
 
 
 @app.route("/event/<event_id>", methods=["GET"])
 def event_by_id(event_id: str):
-    event_row = events_db.events.find_one({"_id": ObjectId(event_id)})
-    return {"ok": True, "event": Event(**event_row).to_dict()}
+    return {"ok": True, "event": Event.find_by_id(event_id)}
 
 
 @app.route("/events", methods=["POST"])
 def create_event():
-    global events_db
-
     event = Event(**request.form.to_dict())
-    if not event.validate():
-        return _error_response(",".join(event.errors))
+    if not event.save():
+        return _error_response(event.errors)
 
-    day_events = events_db.events.find({
-        "date": event.start.strftime("%Y-%m-%d")
-    })
+    return {"ok": True, "event": event.to_dict()}
 
-    if event.is_overlapping(day_events=day_events):
-        return _error_response("Event is overlapping")
-
-    new_id = events_db.events.insert_one(event.to_dict())
-    if not new_id.acknowledged:
-        return _error_response("Cannot save event, please try again ...")
-
-    return {"ok": True, "event": str(new_id.inserted_id)}
 
 @app.route("/events/<event_id>", methods=["PUT"])
 def update_event(event_id):
-    post_data = request.form.to_dict()
-    event_row = events_db.events.find_one({"_id": ObjectId(event_id)})
-    if not event_row:
+    event = Event.find_by_id(event_id)
+    if not event:
         return _error_response("Event ID not found")
-    newEvent = Event(start=post_data['start'], duration=event_row['duration'], description=event_row['description'])
-    newEvent.event_id = str(event_row['_id'])
-    day_events = events_db.events.find({
-        "date": newEvent.start.strftime("%Y-%m-%d")
-    })
 
-    if newEvent.is_overlapping(day_events):
-        return _error_response("Event is overlapping")
+    event.fill(request.form.to_dict())
+    if not event.save():
+        return _error_response(event.errors)
 
-    newEventData = newEvent.to_dict()
-    del newEventData['_id']
-    events_db.events.update_one({"_id": ObjectId(event_id)}, {"$set": newEventData})
-
-    return {"ok": True}
+    return {"ok": True, "event": event.to_dict()}
 
 
 @app.route("/events/<event_id>", methods=['DELETE'])
 def delete_event(event_id):
-    event_row = events_db.events.find_one({"_id": ObjectId(event_id)})
-    if not event_row:
+    event = Event.find_by_id(event_id)
+    if not event:
         return _error_response("Event ID not found")
 
-    events_db.events.delete_one({"_id": ObjectId(event_id)})
+    if not event.delete():
+        return _error_response("An error occurred while deleting the event...")
 
     return {"ok": True}
 
 
 def _error_response(message):
-    return {"ok": False, "error": message}
+    # todo: log messages
 
+    return {"ok": False, "error": message}
--- event.py
+++ event.py
@@ -1,76 +1,106 @@
 from datetime import datetime
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import settings
+
 
 class Event:
-
     FMT_DATE = "%Y-%m-%d"
     FMT_MONTH = "%Y-%m"
     FMT_FULL = "%Y-%m-%d %H:%M:%S"
     FMT_TIME = "%H:%M"
 
-    def __init__(self, start: datetime = None, duration: int = None, description: str = None):
-        if type(start).__name__ == "str":
-            start = datetime.strptime(start, "%Y-%m-%d %H:%M:%S")
+    events_db = MongoClient(settings.MONGO_URI)[settings.MONGO_DB]
+
+    def __init__(self, start: str, end: str, description: str, all_day: bool = False, _id=None):
         self.start = start
-        self.duration = int(duration)
+        self.end = end
         self.description = description
+        self.all_day = all_day
         self.errors = list()
-        self.event_id = None
+        self._id = _id
 
-    @staticmethod
-    def from_db_row(row: dict):
-        event = Event(start=row['start'], duration=row['duration'], description=row['description'])
-        event.event_id = row['_id']
+        self._reformat()
 
-        return event
+    def _reformat(self):
+        if type(self.start).__name__ == "str":
+            self.start = datetime.strptime(self.start, Event.FMT_FULL)
+        if type(self.end).__name__ == "str":
+            self.end = datetime.strptime(self.end, Event.FMT_FULL)
+        if type(self.all_day).__name__ != "bool":
+            self.all_day = self.all_day == "1" or str(self.all_day).lower() == "true"
 
-
-    def to_dict(self) -> dict:
-        ret = {
-            "start": self.start.strftime(Event.FMT_FULL),
-            "month": self.start.strftime(Event.FMT_MONTH),
-            "week": self.start.isocalendar().week,
-            "date": self.start.strftime(Event.FMT_DATE),
-            "time": self.start.strftime(Event.FMT_TIME),
-            "duration": self.duration,
-            "description": self.description,
-        }
-        if self.event_id:
-            ret['_id'] = str(self.event_id)
-
+    def to_dict(self, with_id=True):
+        ret = vars(self)
+        if 'errors' in ret:
+            del ret['errors']
+        if '_id' in ret and not with_id:
+            del ret['_id']
+        if '_id' in ret and not ret['_id']:
+            del ret['_id']
+        if ret.get('_id'):
+            ret['_id'] = str(ret.get('_id'))
         return ret
 
-    def validate(self) -> bool:
+    def save(self):
+        if not self._validate():
+            return False
+        if self._id:
+            result = Event.events_db.events.update_one({"_id": ObjectId(self._id)}, {"$set": self.to_dict(False)})
+            return result.modified_count >= 0
+
+        else:
+            result = Event.events_db.events.insert_one(self.to_dict(False))
+            if not result.inserted_id:
+                return False
+            self._id = result.inserted_id
+
+        return True
+
+    def fill(self, data: dict):
+        for key, val in data.items():
+            setattr(self, key, val)
+
+        self._reformat()
+        return self
+
+    def delete(self):
+        result = Event.events_db.events.delete_one({"_id": ObjectId(self._id)})
+        return result.deleted_count > 0
+
+    @staticmethod
+    def find_by_id(event_id):
+        row = Event.events_db.events.find_one({"_id": ObjectId(event_id)})
+        if not row:
+            return None
+        return Event(**row)
+
+    @staticmethod
+    def find(start: datetime, end: datetime):
+        results = Event.events_db.events.find({
+            "start": {
+                "$gte": start,
+            },
+            "end": {
+                "$lte": end
+            }
+        })
+
+        return map(lambda row: Event(**row), results)
+
+    def _validate(self) -> bool:
         valid = True
         if self.start is None:
-            self.errors.append("start_date has to be set")
+            self.errors.append("start date/time has to be set")
             valid = False
-        if self.duration is None or self.duration <= 0:
-            self.errors.append("duration has to be set to a positive number (minutes)")
+        if self.end is None:
+            self.errors.append("end date/time has to be set")
             valid = False
         if not self.description or not len(self.description):
             self.errors.append("description has to be set")
             valid = False
 
-        return valid
+        return valid and not self._is_overlapping([])
 
-    def time_int(self) -> int:
-        return int(self.to_dict().time.replace(":", ""))
-
-    def is_overlapping(self, day_events: list) -> bool:
-        # find an event later than this event's time
-        # and check if its overlapping
-        for event in day_events:
-            if str(event['_id']) == self.event_id:
-                continue
-            event['start'] = datetime.strptime(event['start'], Event.FMT_FULL)
-            if event['start'] == self.start:
-                return True
-            if self.start.timestamp() + self.duration*60 > event['start'].timestamp() and event['start'] > self.start:
-                return True
-            if event['start'].timestamp() + self.duration*60 > self.start.timestamp() and event['start'] < self.start:
-                return True
-
-
+    def _is_overlapping(self, day_events: list) -> bool:
         return False
-
-
